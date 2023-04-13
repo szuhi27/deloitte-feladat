ec7ccd4a6deb9625351b25267e2cc2e6a00db2f1
David <szuhid@gmail.com>
2023-04-13 14:15:14 +0200

    nullinput handling added

--- deloitte-feladat/Program.cs
+++ deloitte-feladat/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using deloitte_feladat;
+
+internal class Program
+{
+
+    static List<Tuple<int, string>> datasList = new(7);
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine($"Add an item to the list: (current items: {datasList.Count}/7)");
+
+        try
+        {
+            GetInput();
+        }
+        catch (WrongInputException e) 
+        { 
+            Console.WriteLine(e.Message); 
+        }
+    }
+
+    static void GetInput()
+    {
+        var input = Console.ReadLine();
+
+        if(string.IsNullOrEmpty(input) )
+        {
+            throw new WrongInputException("Input can't be null");
+        }
+    }
+
+
+}
---
+++ deloitte-feladat/WrongInputException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace deloitte_feladat
+{
+    public class WrongInputException : Exception
+    {
+        public WrongInputException() { }
+
+        public WrongInputException(string message) : base(message) { }
+    }
+}
